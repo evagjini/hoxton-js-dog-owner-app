1393ba9ccec4099bcc26279c9ba62f469beb6a6c
Fevronia Gjini <fevronigjini1@gmail.com>
2022-06-23 23:47:31 +0200

    final final

--- src/index.js
+++ src/index.js
@@ -49,6 +49,7 @@ for(let dog of window.dogs) {
 
         section.append(img)
 
+
         let div = document.createElement('div')
         div.className = 'main__dog-section__desc'
 
@@ -85,3 +86,98 @@ for(let dog of window.dogs) {
 
     ul.append(li)
 }
+
+
+// I need this element to add <li>s to it, one for each dog
+let dogList = document.querySelector('.dogs-list')
+
+// I need this element to display the selected dog
+let dogSection = document.querySelector('.main__dog-section')
+
+function addDogListItem(dog) {
+  let dogLi = document.createElement('li')
+  dogLi.className = 'dogs-list__button'
+  dogLi.textContent = dog.name
+
+  dogLi.addEventListener('click', function () {
+    displayDog(dog)
+  })
+
+  dogList.append(dogLi)
+}
+
+function addDogListItems() {
+  for (let dog of data) {
+    addDogListItem(dog)
+  }
+}
+
+function displayDog(dog) {
+  // clear all the contents of the section
+  dogSection.textContent = ''
+
+  let nameEl = document.createElement('h2')
+  nameEl.textContent = dog.name
+
+  let imageEl = document.createElement('img')
+  imageEl.src = dog.image
+  imageEl.alt = ''
+
+  let descEl = document.createElement('div')
+  descEl.className = 'main__dog-section__desc'
+
+  let bioTitleEl = document.createElement('h3')
+  bioTitleEl.textContent = 'Bio'
+
+  let bioContentEl = document.createElement('p')
+  bioContentEl.textContent = dog.bio
+
+  let isNaughtyP = document.createElement('p')
+  let isNaughtyEm = document.createElement('em')
+  isNaughtyEm.textContent = 'Is naughty?'
+  isNaughtyP.append(isNaughtyEm, dog.isGoodDog ? ' No' : ' Yes')
+
+  let goodDogButton = document.createElement('button')
+  goodDogButton.className = 'main__dog-section__btn'
+  goodDogButton.textContent = dog.isGoodDog ? 'Good dog!' : 'Bad dog!'
+  goodDogButton.addEventListener('click', function () {
+    // update state
+    dog.isGoodDog = !dog.isGoodDog
+    // render
+    displayDog(dog)
+  })
+
+  descEl.append(bioTitleEl, bioContentEl)
+  dogSection.append(nameEl, imageEl, descEl, isNaughtyP, goodDogButton)
+}
+
+// function displayForm() {
+//   dogSection.textContent = ''
+//   // <h2>Add a new Dog</h2>
+//   // <form class="form">
+
+//   //   <label for="name">Dog's name</label>
+//   //   <input type="text" id="name" name="name">
+
+//   //   <label for="image">Dog's picture</label>
+//   //   <input type="url" id="image" name="image">
+
+//   //   <label for="bio">Dog's bio</label>
+//   //   <textarea rows="5" id="bio" name="bio"></textarea>
+
+//   //   <input type="submit" id="submit" name="submit" value="Let's add a dog!" class="form__button">
+//   // </form>
+
+//   const dog = {
+//     name: 'My New Dog',
+//     bio: `I'm a new dog, I promise I'll behave!`,
+//     isGoodDog: true,
+//     image:
+//       'https://images.pexels.com/photos/825949/pexels-photo-825949.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+//   }
+//   data.push(dog)
+//   addDogListItem(dog)
+// }
+
+
+// addDogListItems()
